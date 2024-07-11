ac6b1a029b15d861c18ee14a4bee796c46009b36
tbenbow-booth <tbenbow@chicagobooth.edu>
2024-07-11 04:51:48 +0000

    Final 100

--- app.rb
+++ app.rb
@@ -2,8 +2,56 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rat)
+end
+
+get ("/rock") do
+  moves = ["rock" , "paper" , "scissors"]
+
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "tied"
+  elsif @comp_moves == "paper"
+    @outcome = "lost"
+  else @comp_moves == "scissors"
+    @outcome = "won"
+  end
+
+  erb(:zebra)
+
+end
+
+get ("/paper") do
+  moves = ["rock" , "paper" , "scissors"]
+
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "won"
+  elsif @comp_moves == "paper"
+    @outcome = "tied"
+  else @comp_moves == "scissors"
+    @outcome = "lost"
+  end
+
+  erb(:turtle)
+
+end
+
+get ("/scissors") do
+  moves = ["rock" , "paper" , "scissors"]
+
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "lost"
+  elsif @comp_moves == "paper"
+    @outcome = "won"
+  else @comp_moves == "scissors"
+    @outcome = "tied"
+  end
+
+  erb(:lion)
+
 end
